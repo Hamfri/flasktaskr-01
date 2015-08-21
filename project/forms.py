c2a8df470b876f31a6df56407d96f7f071683b39
Hamfri <kibzham@gmail.com>
2015-08-21 22:17:03 +0300

    Removed Vanilla SQL and introduced SQLAlchemy to manage database transactions also created the Register view

--- project/forms.py
+++ project/forms.py
@@ -1,14 +1,15 @@
 # project/forms.py
 
 from flask_wtf import Form
-from wtforms import StringField, DateField, IntegerField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, IntegerField, SelectField, \
+PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class AddTaskForm(Form):
     task_id = IntegerField()
     name = StringField('Task Name', validators=[DataRequired()])
     due_date = DateField(
-        'Date Due (mm/dd/yyy)',
+        'Date Due (mm/dd/yyyy)',
         validators=[DataRequired()], format='%m%d%Y'
         )
     priority = SelectField('Priority',
@@ -19,3 +20,34 @@ class AddTaskForm(Form):
                             ]
                            )
     status = IntegerField('Status')
+
+class RegisterForm(Form):
+    name = StringField('Username',
+                       validators=[DataRequired(), Length(min=6, max=25)]
+                       )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired(), Length(min=6, max=40)]
+    )
+    confirm = PasswordField(
+        'Repeat Password',
+        validators=[DataRequired(), EqualTo('password',
+                                            message='Passwords must match')]
+    )
+    
+class LoginForm(Form):
+    name = StringField(
+        'Username',
+        validators=[DataRequired()]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
+    
+    
+
