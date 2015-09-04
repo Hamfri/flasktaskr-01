21aa3fa24ab671b063269d0f560faa059b795f9a
Hamfri <kibzham@gmail.com>
2015-09-04 16:56:03 +0300

    added fabfile for automated deployment

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,10 @@ from fabric.api import local, settings, abort
 from fabric.contrib.console import confirm
 
 def test():
-    local("nosetests --with-coverage --cover-erase --cover-package=project")
+    with settings(warn_only=True):
+        result = local("nosetests -v", capture=True)
+    if result.failed and not confirm("Tests failed. Continue?"):
+        abort("Aborted at user request.")
 
 def commit():
     message = raw_input("Enter a git commit message: ")
