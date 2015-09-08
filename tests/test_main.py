41ccb0c69ee58192862821418503cef51ea59427
Hamfri <kibzham@gmail.com>
2015-09-08 15:03:18 +0300

    Deactivated automated deployments

--- tests/test_main.py
+++ tests/test_main.py
@@ -20,8 +20,8 @@ class MainTests(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
-        #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+        os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
         db.create_all()
         
