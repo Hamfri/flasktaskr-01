b14eacca890a06cc89b2be63b961f911192f6c10
Hamfri <kibzham@gmail.com>
2015-09-08 14:46:43 +0300

    Automated heroku deployments using travis

--- tests/test_users.py
+++ tests/test_users.py
@@ -19,8 +19,8 @@ class AllTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ \
-        os.path.join(basedir, TEST_DB)
+        #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir, TEST_DB)
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
         self.app = app.test_client()
         db.create_all()
         
